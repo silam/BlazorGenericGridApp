edc21585c7f62b195c0d6a9288e203734d81e3f7
Si Lam <silam@hotmail.com>
2022-11-08 15:04:51 -0600

    Pagination

---
+++ UIRazorClassLibraryComponent/Column.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIRazorClassLibraryComponent
+{
+    public partial class Column<TItem>
+    {
+        [Parameter]
+        public string Name { get; set; } // headeer column name
+        [Parameter]
+        public string Label { get; set; } // header label
+
+        [CascadingParameter]
+        public TItem Item { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
---
+++ UIRazorClassLibraryComponent/Grid.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIRazorClassLibraryComponent
+{
+    public partial class Grid<TItem>
+    {
+        [Parameter]
+        public IList<TItem> Items { get; set; }
+
+        [Parameter]
+
+        public RenderFragment<TItem>? Columns { get; set; }
+    }
+}
